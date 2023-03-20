4862130a8a68157b1237dfca996da425e632d850
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-03-20 18:16:30 +0300

    changed view adding products to cart

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.CartView.as_view(), name='cart_detail'),
-    path('add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
+    path('add/<int:product_id>/', views.CartAddFormView.as_view(), name='cart_add'),
     path('remove/<int:product_id>/', views.CartDeleteView.as_view(), name='cart_remove')
 ]
--- src/cart/views.py
+++ src/cart/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, FormView
 
 from .cart import Cart
 from .forms import CartAddProductForm
 from shop.models import Product
 
 
-class CartAddView(CreateView):
-    def post(self, request, *args, **kwargs):
-        cart = Cart(request)
+class CartAddFormView(FormView):
+    form_class = CartAddProductForm
+
+    def form_valid(self, form):
+        cart = Cart(self.request)
         product = get_object_or_404(Product, id=self.kwargs['product_id'])
-        form = CartAddProductForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            cart.add(product=product,
-                     quantity=cd['quantity'],
-                     update_quantity=cd['update'])
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+        return redirect('cart_detail')
+
+    def form_invalid(self, form):
         return redirect('cart_detail')
 
 
