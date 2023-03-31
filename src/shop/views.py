0a14b24300cff872d1da608114e7f3dd0727dc7a
Mikhail Profatsky <mixail.rostov04@gmail.com>
2023-03-31 19:57:42 +0300

    added sidebar with filters for product catalog

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Q, Min, Max
 from django.views import generic
 
 from cart.forms import CartAddProductForm
-from .models import Product
+from .models import Product, Brand
 
 
 class ProductListView(generic.ListView):
@@ -10,10 +11,43 @@ class ProductListView(generic.ListView):
     paginate_by = 8
 
     def get_queryset(self):
-        return Product.objects.select_subclasses().filter(category__slug=self.kwargs['category_slug'])
+        queryset = Product.objects.select_subclasses().filter(category__slug=self.kwargs['category_slug'])
+
+        if min_price := self.request.GET.get('min-price'):
+            queryset = queryset.filter(Q(price__gte=min_price) | Q(discount_price__gte=min_price))
+        if max_price := self.request.GET.get('max-price'):
+            queryset = queryset.filter(Q(price__lte=max_price) | Q(discount_price__lte=max_price))
+        if brands := self.request.GET.getlist('brand'):
+            queryset = queryset.filter(brand__slug__in=brands)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
+        queryset = self.get_queryset()
+
+        if min_price := self.request.GET.get('min-price'):
+            context['min_price'] = min_price
+        else:
+            context['min_price'] = int(min(
+                queryset.aggregate(Min('price'))['price__min'],
+                discount_price if (
+                    discount_price := queryset.aggregate(Min('discount_price'))['discount_price__min']
+                ) else 0
+            ))
+
+        if max_price := self.request.GET.get('max-price'):
+            context['max_price'] = max_price
+        else:
+            context['max_price'] = int(max(
+                queryset.aggregate(Max('price'))['price__max'],
+                discount_price if (
+                    discount_price := queryset.aggregate(Max('discount_price'))['discount_price__max']
+                ) else 0
+            ))
+
+        context['brands'] = Brand.objects.all()
+        context['checked_brands'] = self.request.GET.getlist('brand')
         context['cart_form'] = CartAddProductForm()
         return context
 
